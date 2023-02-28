be7f3aacaac81eb819a72e212f3d5157c52dace3
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2023-02-28 12:34:10 +0700

    Add Fix Code

--- app-qr.py
+++ app-qr.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import streamlit as st
+import json
 
 image = st.camera_input("Show QR code")
 
@@ -10,25 +11,17 @@ if image is not None:
     detector = cv2.QRCodeDetector()
     data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
 
-    data_makanan = [
-        {
-            'code': 12345678,
-            'nama_makanan': 'seblak madya',
-            'stok': 5
-        },
-        {
-            'code': 12345679,
-            'nama_makanan': 'Bakso',
-            'stok': 3
-        }
-    ]
+    # load data makanan dari file json
+    with open('food-api.json') as json_file:
+        data_makanan = json.load(json_file)
+
 
     data_makanan_temp = {}
     st.write("Kode Qr : ", data)
 
     # cari pada data_makanan
     for i in data_makanan:
-        if i['code'] == int(data):
+        if i['code'] == str(data):
             data_makanan_temp = {
                 'code': i['code'],
                 'nama_makanan': i['nama_makanan'],
@@ -37,16 +30,22 @@ if image is not None:
             break
 
     st.write("Data Makanan sebelum dibeli", data_makanan_temp)
-
-    # cari pada data_makanan
-    for i in data_makanan:
-        if i['code'] == int(data):
-            stok = i['stok'] - 1
-            data_makanan_temp = {
-                'code': i['code'],
-                'nama_makanan': i['nama_makanan'],
-                'stok': stok
-            }
-            break
     
-    st.write("Data Makanan Setelah dibeli", data_makanan_temp)
+     # kurangin stok pada json data_makanan_temp 
+    if data_makanan_temp['stok'] > 0:
+        data_makanan_temp['stok'] = data_makanan_temp['stok'] - 1
+
+    # update data makanan
+    for i in data_makanan:
+        if i['code'] == str(data):
+            if i['stok'] > 0:
+                i['stok'] = data_makanan_temp['stok']
+                # simpan data makanan ke file json
+                with open('food-api.json', 'w') as outfile:
+                    json.dump(data_makanan, outfile)
+                st.write("Data Makanan Setelah dibeli", data_makanan_temp)
+            else:
+                st.write("Stok habis")
+            break
+
+    
